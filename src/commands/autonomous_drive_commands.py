adc0fb704460a6e299092ee8e72c18c6192c1ce5
Retrojays Team 94 <teamfrc94@gmail.com>
2023-03-11 10:20:42 -0500

    error: investigate auto mode attribute error

--- src/commands/autonomous_drive_commands.py
+++ src/commands/autonomous_drive_commands.py
@@ -12,6 +12,8 @@ class MoveFromLine(CommandGroupBase):
     _DRIVE_SPEED = "DRIVE_SPEED"
     _DRIVE_TIME = "DRIVE_TIME"
 
+    _drivetrain: Drivetrain = None
+
     def __init__(
             self,
             drivetrain: Drivetrain,
@@ -21,16 +23,20 @@ class MoveFromLine(CommandGroupBase):
         super().__init__()
         self._config = config
         self._load_config(config)
-        self._initialize_commands()
+        self._initialize_commands(drivetrain)
+        print('move from line: drivetrain - ' + drivetrain)
         self._drivetrain = drivetrain
 
     def _load_config(self, parser: ConfigParser):
         self._drive_speed = parser.getfloat(self._SECTION, self._DRIVE_SPEED)
         self._drive_time = parser.getfloat(self._SECTION, self._DRIVE_TIME)
 
-    def _initialize_commands(self):
-        command = DriveTime(self._drivetrain, self._drive_time, self._drive_speed)
-        self.addSequential(command)
+    def _initialize_commands(self, drivetrain: Drivetrain) -> None:
+        if drivetrain:
+            command = DriveTime(drivetrain, self._drive_time, self._drive_speed)
+            self.addSequential(command)
+        else:
+            print('Drivetrain not found for MoveLine')
 
 
 class DriveToWall(CommandGroupBase):
